383d8c1793f028621faba7bc7a9eace6e9e0e0dd
Riddhi <riddhijaju123@gmail.com>
2022-08-23 23:00:33 -0400

    initial

---
+++ src/main/java/com/example/passportStatusTrackingSystem/PassportStatusTrackingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.passportStatusTrackingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassportStatusTrackingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassportStatusTrackingSystemApplication.class, args);
+	}
+
+}
